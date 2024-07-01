07fdc4d5f4db169af80b20eb1b3f108ce7112e63
dev.roshan <roshanrishantha16@gmail.com>
2024-07-02 03:52:25 +0530

    Example 1

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, SectionList, StatusBar, RefreshControl } from 'react-native';
 
-export default function App() {
+
+const App = () => {
+// const [items, setItems] = useState([])
+const [refreshing, setRefreshing] = useState(false);
+const [data, setData] = useState([
+  {
+    title: 'Title 1',
+    data: ['Item 1']
+  },
+  {
+    title: 'Title 2',
+    data: ['Item 2', 'Item 3']
+  },
+]);
+let DATA = [
+  {
+        title: 'Title 1',
+        data: ['Item 1']
+      },
+      {
+        title: 'Title 2',
+        data: ['Item 2', 'Item 3']
+      },
+      
+];
+
+const onRefresh = () => {
+  setRefreshing(true)
+  const newData = [
+    ...data,
+    { title: 'Title 3', data: ['Item 4', 'Item 5'] }
+  ];
+  setData(newData);
+  setRefreshing(false)
+}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SectionList
+    style={styles.container}
+      keyExtractor={(item, index) => item+index}
+      sections={data}
+      renderItem={({item}) => (
+        <View style={styles.item}>
+          <Text style={styles.title}>{item}</Text>
+        </View>
+      )}
+
+      renderSectionHeader={({section: {title}}) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+
+      refreshControl={
+      <RefreshControl 
+        refreshing={refreshing}
+        onRefresh={onRefresh}
+      />}
+
+     />
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+    
+    
   },
-});
+  item: {
+    
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+  },
+})
+export default App;
